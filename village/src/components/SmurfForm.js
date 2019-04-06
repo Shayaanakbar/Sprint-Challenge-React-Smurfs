2f9dc500f2f96c39528e3e8338c287998e900b5e
shayaanakbar <shayaanakbar1106@gmail.com>
2019-04-06 11:58:56 -0500

    Added Router functionality

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -17,8 +17,12 @@ class SmurfForm extends Component {
       const [name, age, height] = [this.state.name, Number(this.state.age), ];
 
       axios.post('http://localhost:3333/smurfs', {name, age, height })
-          .then(res => this.props.fetchSmurfs())
+          .then(res => {
+              this.props.fetchSmurfs();
+              this.props.history.push('/');
+          })
           .catch(err => console.log(err));
+
 
     this.setState({
       name: '',
@@ -46,6 +50,7 @@ class SmurfForm extends Component {
             placeholder="age"
             value={this.state.age}
             name="age"
+            type="number"
           />
           <input
             onChange={this.handleInputChange}
